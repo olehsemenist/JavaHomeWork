86c475730d70b8425599b23cc681575dfb62876f
Администратор <oleg.semenist@privatbank.ua>
2021-10-14 23:27:21 +0300

    init commit

---
+++ src/com/pb/semenist/hw2/Calculator.java
@@ -0,0 +1,9 @@
+package com.pb.semenist.hw2;
+
+public class Calculator {
+    public static void main(String[] args) {
+        int x = 9;
+        int y = 0;
+        String sign = "+"; // + - * /
+    }
+}
